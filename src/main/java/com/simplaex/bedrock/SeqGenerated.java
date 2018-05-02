ae38b179c2c3b9ef95c96ea5c05abf4d8b5b2608
Julian Fleischer <julian@simplaex.com>
2018-05-02 09:05:33 +0200

    2018.05.03

--- src/main/java/com/simplaex/bedrock/SeqGenerated.java
+++ src/main/java/com/simplaex/bedrock/SeqGenerated.java
@@ -2,34 +2,25 @@ package com.simplaex.bedrock;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
+import java.lang.reflect.Array;
 import java.util.Comparator;
-import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.function.IntFunction;
 
 class SeqGenerated<E> extends Seq<E> {
 
   private final IntFunction<E> backingFunction;
   private final int length;
-  private SeqSimple<E> materializedSeq = null;
 
   SeqGenerated(@Nonnull final IntFunction<E> function, @Nonnegative final int length) {
     this.backingFunction = function;
     this.length = length;
   }
 
-  private void materialize() {
-    if (materializedSeq == null) {
-      final Object[] array = new Object[length];
-      for (int i = 0; i < length; i += 1) {
-        array[i] = backingFunction.apply(i);
-      }
-      materializedSeq = Seq.ofArrayZeroCopyInternal(array);
-    }
-  }
-
+  @SuppressWarnings("ConstantConditions")
   @Override
   public E get(@Nonnegative final int index) {
-    if (index >= length) {
+    if (index >= length || index < 0) {
       throw new IndexOutOfBoundsException();
     }
     return backingFunction.apply(index);
@@ -38,22 +29,29 @@ class SeqGenerated<E> extends Seq<E> {
   @Nonnull
   @Override
   public Seq<E> sortedBy(@Nonnull final Comparator<? super E> comparator) {
-    materialize();
-    return materializedSeq.sortedBy(comparator);
+    return trimmedToSize().sortedBy(comparator);
   }
 
   @Nonnull
   @Override
-  public E[] toArray(@Nonnull final Class<E> clazz) {
-    materialize();
-    return materializedSeq.toArray(clazz);
+  public E[] toArray(@Nonnull final Class<E> evidence) {
+    Objects.requireNonNull(evidence);
+    final int length = size();
+    @SuppressWarnings("unchecked") final E[] array = (E[]) Array.newInstance(evidence, length);
+    for (int i = 0; i < length; i += 1) {
+      array[i] = get(i);
+    }
+    return array;
   }
 
   @Nonnull
   @Override
   public Object[] toArray() {
-    materialize();
-    return materializedSeq.toArray();
+    final Object[] array = new Object[size()];
+    for (int i = 0; i < size(); i += 1) {
+      array[i] = backingFunction.apply(i);
+    }
+    return array;
   }
 
   @Override
@@ -64,36 +62,41 @@ class SeqGenerated<E> extends Seq<E> {
   @Nonnull
   @Override
   public Seq<E> reversed() {
-    materialize();
-    return materializedSeq.reversed();
+    return trimmedToSize().reversed();
   }
 
   @SuppressWarnings("unchecked")
   @Nonnull
   @Override
   public Seq<E> sorted() {
-    materialize();
-    return materializedSeq.sortedInternal();
+    return trimmedToSize().sorted();
   }
 
   @Nonnull
   @Override
   public Seq<E> trimmedToSize() {
-    materialize();
-    return materializedSeq.trimmedToSize();
+    return Seq.ofArrayZeroCopyInternal(toArray());
   }
 
   @Nonnull
   @Override
   public Seq<E> subSequence(final int beginOffset, final int endOffset) {
-    materialize();
-    return materializedSeq.subSequence(beginOffset, endOffset);
+    final int begin = Math.max(0, beginOffset);
+    final int end = Math.min(length(), endOffset);
+    final int len = end - begin;
+    if (len <= 0) {
+      return empty();
+    }
+    final Object[] array = new Object[len];
+    for (int i = 0; i < len; i += 1) {
+      array[i] = get(begin + i);
+    }
+    return Seq.ofArrayZeroCopyInternal(array);
   }
 
   @Nonnull
   @Override
   public Seq<E> subSequenceView(final int beginOffset, final int endOffset) {
-    materialize();
-    return materializedSeq.subSequenceView(beginOffset, endOffset);
+    return subSequence(beginOffset, endOffset);
   }
 }
